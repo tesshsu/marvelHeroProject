0fb71f9310992e095a7b723c9c08d84b1782d4d0
tess <tess.hsu@gmail.com>
2023-03-15 22:00:18 +0100

    fix pagination

--- server/src/server.js
+++ server/src/server.js
@@ -2,24 +2,37 @@ const express = require('express')
 const axios = require('axios')
 const md5 = require('md5')
 const config = require('./config')
+const mcache = require('memory-cache')
 
 const app = express()
 
-app.get('/api/characters', async (req, res) => {
-    const { page = 1, limit = 20 } = req.query
-    const ts = new Date().getTime().toString()
-    const hash = md5(ts + config.marvel.privateKey + config.marvel.publicKey)
-    const url = `${config.marvel.baseUrl}/characters?ts=${ts}&apikey=${config.marvel.publicKey}&hash=${hash}`
+const CACHE_DURATION = 120; // in seconds
 
+
+app.get('/api/characters', async (req, res) => {
     try {
-        const response = await axios.get(url)
-        const { total, count, results } = response.data.data
+        const { page = 1, limit = 20 } = req.query
+        const offset = (page - 1) * limit;
+        const reqKey = `marvel_characters_offset_${offset}_limit_${limit}`;
+        let marvelCharactersData = mcache.get(reqKey);
+
+        // Fetch data if not in cache
+        if(!marvelCharactersData){
+            const ts = new Date().getTime().toString()
+            const hash = md5(ts + config.marvel.privateKey + config.marvel.publicKey)
+            const url = `${config.marvel.baseUrl}/characters?ts=${ts}&apikey=${config.marvel.publicKey}&hash=${hash}&offset=${offset}&limit=${limit}`
+            const response = await axios.get(url)
+            marvelCharactersData = response.data.data;
+            mcache.put(reqKey, marvelCharactersData, CACHE_DURATION * 1000);
+        }
+
+        const { total, count, results } = marvelCharactersData;
         const totalPages = Math.ceil(total / limit)
         const nextPage = page < totalPages ? +page + 1 : null
         const prevPage = page > 1 ? +page - 1 : null
         const pagination = { totalPages, nextPage, prevPage }
-        console.log('response result: ', response.data.data)
-        res.json({ total, count, results, pagination })
+        //console.log('response result: ', marvelCharactersData)
+        res.json({ total, count, pagination , results})
     } catch (error) {
         console.error(error)
         await res.json(error)
