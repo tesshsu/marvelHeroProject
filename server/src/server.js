e7a9701066408889cb9895cfad8679cf0543ef0f
tess <tess@furuke.com>
2023-03-16 09:01:05 +0100

    [task-3] add unit test route api

--- server/src/server.js
+++ server/src/server.js
@@ -5,21 +5,24 @@ const config = require('./config')
 
 const app = express()
 
+/* A middleware that parses the body of the request and makes it available in the req.body object. */
+app.use(express.json());
+
 app.get('/api/characters', async (req, res) => {
-    const { page = 1, limit = 20 } = req.query
+    const { page = 1, limit = 5 } = req.query
     const ts = new Date().getTime().toString()
     const hash = md5(ts + config.marvel.privateKey + config.marvel.publicKey)
     const url = `${config.marvel.baseUrl}/characters?ts=${ts}&apikey=${config.marvel.publicKey}&hash=${hash}`
 
     try {
         const response = await axios.get(url)
-        const { total, count, results } = response.data.data
-        const totalPages = Math.ceil(total / limit)
+        const count = response.data.data.count;
+        const results = response.data.data.results;
+        const totalPages = Math.ceil(count / limit)
         const nextPage = page < totalPages ? +page + 1 : null
         const prevPage = page > 1 ? +page - 1 : null
         const pagination = { totalPages, nextPage, prevPage }
-        console.log('response result: ', response.data.data)
-        res.json({ total, count, results, pagination })
+        res.json({ count ,pagination, results })
     } catch (error) {
         console.error(error)
         await res.json(error)
@@ -31,3 +34,5 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
+
+module.exports = app;
